44d9182da6edc2ad8b5a6f33ba1bea49e48b2eb9
ArunP22 <arunpujari01@gmail.com>
2024-04-08 00:32:17 +0530

    Added homeLoanCalculator

--- gitDemo3/Main.java
+++ gitDemo3/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
         System.out.println("Compound Interest "+ CompoundInterestCalculator.calculate(100000,8,5));
         System.out.println("SIP return : " + SIPCalculator.calculate(10000,12,12,20));
-
+        HomeLoanCalculator.homeLoanCalculator(
+                3600000,
+                600000,
+                9,
+                30
+        );
     }
 }
